5bb1770ac4a0c74e584ea99980576e05329d0ac4
crosbygs <crosby.greg@gmail.com>
2020-02-14 18:49:01 +0000

    New Char Picture module

---
+++ CharPicture.py
@@ -0,0 +1,15 @@
+grid = [['.', '.', '.', '.', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['.', 'O', 'O', 'O', 'O', 'O'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '.']]
+
+for yCounter in range(len(grid[0])):
+  for xCounter in range(len(grid)):
+    print(grid[xCounter][yCounter],end='')
+  print()
+
